19bdc80eb38f7400ce53c65662b89bfa892550e0
Cardinal <cardinalby@gmail.com>
2022-05-13 14:59:07 +0300

    rm unused manifest.d.ts

--- declarations/manifest.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IManifestObject {
-    manifest_version: number
-    name: string;
-    version: string;
-    author?: string,
-    description?: string;
-    [key: string]: any;
-}
